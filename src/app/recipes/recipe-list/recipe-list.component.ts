a7a2658f69d94b65a34d2948dabe4ee1f0b0060b
Isabella Beatriz <isabellabeatrizn@outlook.com>
2020-03-22 10:54:52 -0300

    Building Recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is a text", "https://img.itdg.com.br/tdg/images/recipes/000/001/545/282163/282163_original.jpg"),
+    new Recipe("A test recipe", "This is a text", "https://img.itdg.com.br/tdg/images/recipes/000/001/545/282163/282163_original.jpg")
+
+  ];
 
   constructor() { }
 
